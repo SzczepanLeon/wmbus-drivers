26021ebd02c2f1636499e43373bc7cd7f29f1902
aurban6 <aurban6@gmail.com>
2023-11-22 17:40:22 +0100

    update volume_flow_lh

--- driver_sharky774.h
+++ driver_sharky774.h
@@ -19,7 +19,7 @@ struct Sharky774 : Driver
     add_to_map(ret_val, "total_energy_consumption_gj", this->get_total_energy_consumption_gj(telegram));
     add_to_map(ret_val, "power_kw", this->get_0C2B(telegram));
     add_to_map(ret_val, "total_volume_m3", this->get_0C13(telegram));
-    add_to_map(ret_val, "volume_flow_l3h", this->get_0B3B(telegram));
+    add_to_map(ret_val, "volume_flow_lh", this->get_0B3B(telegram));
     add_to_map(ret_val, "flow_temperature_c", this->get_0A5A(telegram));
     add_to_map(ret_val, "return_temperature_c", this->get_0A5E(telegram));
     add_to_map(ret_val, "operating_time_d", this->get_0B26(telegram));
