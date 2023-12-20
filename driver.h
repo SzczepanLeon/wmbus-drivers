346628f8ed8ef0563e0a578d881a3d4303f7183b
wonsky007 <98755579+wonsky007@users.noreply.github.com>
2023-12-20 11:41:25 +0100

    Update driver.h
    Added get_0C09 for Hydrocal M4

--- driver.h
+++ driver.h
@@ -95,6 +95,27 @@ protected:
     return ret_val;
   };
 
+
+  esphome::optional<double> get_0C09(std::vector<unsigned char> &telegram) {
+    esphome::optional<double> ret_val{};
+    uint32_t usage = 0;
+    size_t i = 11;
+    uint32_t total_register = 0x0C09;
+    while (i < telegram.size()) {
+      uint32_t c = (((uint32_t)telegram[i+0] << 8) | ((uint32_t)telegram[i+1]));
+      if (c == total_register) {
+        i += 2;
+        usage = bcd_2_int(telegram, i, 4);
+        // in kWh
+        ret_val = usage / 3.6;
+        ESP_LOGVV(TAG, "Found register '0C09' with '%d'->'%f'", usage, ret_val.value());
+        break;
+      }
+      i++;
+    }
+    return ret_val;
+  };
+
   esphome::optional<double> get_0C0D(std::vector<unsigned char> &telegram) {
     esphome::optional<double> ret_val{};
     uint32_t usage = 0;
