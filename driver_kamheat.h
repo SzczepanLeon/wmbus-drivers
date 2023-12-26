2fe1b6bca786536bc8efbd08d7a89e180c1197ea
Jacman777 <149330119+Jacman777@users.noreply.github.com>
2023-12-26 17:08:03 +0100

    Update driver_kamheat.h

--- driver_kamheat.h
+++ driver_kamheat.h
@@ -16,7 +16,7 @@ struct Kamheat: Driver
   virtual esphome::optional<std::map<std::string, float>> get_values(std::vector<unsigned char> &telegram) override {
     std::map<std::string, float> ret_val{};
 
-    add_to_map(ret_val, "total_energy_consumption_kwh", this->get_current_heating_GJ(telegram));
+    add_to_map(ret_val, "total_energy_consumption_gj", this->get_current_heating_GJ(telegram));
     if (ret_val.size() > 0) {
       return ret_val;
     }
@@ -30,7 +30,7 @@ private:
       esphome::optional<float> ret_val{};
       size_t i = 19;
 
-      ret_val = (((uint32_t)telegram[i+4] << 8) + (uint32_t)telegram[i+3]);
+      ret_val = (((uint32_t)telegram[i+6] << 32) + (uint32_t)telegram[i+5] << 16) + (uint32_t)telegram[i+4] << 8) + (uint32_t)telegram[i+3]);
 
       return ret_val;
     };
