769c318df4139c2cc4cc6d6405e601c117cbad1b
Szczepan <szczepan.staszak@gmail.com>
2024-02-08 12:16:32 +0100

    Small fix

--- driver.h
+++ driver.h
@@ -547,7 +547,7 @@ struct Driver
           i += 2;
           usage = bcd_2_int(telegram, i, 3);
           // in kW
-          ret_val = usage / 1000.0;
+          ret_val = usage / 10.0;
           ESP_LOGVV(TAG, "Found register '0B2D' with '%d'->'%f'", usage, ret_val.value());
           break;
         }
@@ -606,8 +606,8 @@ struct Driver
         if (c == total_register) {
           i += 2;
           usage = (((uint32_t)telegram[i+2] << 16) | ((uint32_t)telegram[i+1] << 8)  | ((uint32_t)telegram[i+0]);
-          // in l
-          ret_val = usage / 10.0;
+          // in m3
+          ret_val = usage / 1000.0;
           ESP_LOGVV(TAG, "Found register '4315' with '%d'->'%f'", usage, ret_val.value());
           break;
         }
